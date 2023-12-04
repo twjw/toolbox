095ac1a2ab59bc10a676fe06c0e15336ec90b6d2
twjw <aaaawjwa@gmail.com>
2023-12-04 14:30:06 +0800

    feat: create-fetch2 基本功能完成(攔截器+四種method call)

--- packages/common/create-fetch2/index.ts
+++ packages/common/create-fetch2/index.ts
@@ -22,7 +22,7 @@ module Fetch2 {
             : never
     : never
 
-  export type RequestInit = NodeJS.fetch.RequestInit & {
+  export type RequestInit = Omit<NodeJS.fetch.RequestInit, 'body'> & {
     body?: object
     params?: object
     resType?: ResType
@@ -30,26 +30,46 @@ module Fetch2 {
 
   export type ResType = 'arrayBuffer' | 'blob' | 'formData' | 'json' | 'text'
 
-  export type InterceptorUseCallback = <R>(res: Response) => R
+  export type InterceptorUseRequestCallback = (res: Config) => Config
 
-  export type InterceptorUse = (callback: InterceptorUseCallback) => void
+  export type InterceptorUseRequest = (callback: InterceptorUseRequestCallback) => void
+
+  export type InterceptorUseResponseCallback = (res: InterceptorResponse | { req: ResReq }) => any
+
+  export type InterceptorUseResponse = (callback: InterceptorUseResponseCallback) => void
+
+  export type InterceptorResponse = Response & { data: any, req: ResReq }
+  export type InterceptorErrorResponse = { req: ResReq }
+
+  export type Config = Fetch2.RequestInit
+    & {
+    url: string
+  }
+
+  export type Request = Omit<Config, 'body'> & { body: NodeJS.fetch.RequestInit['body'] }
+
+  export type ResReq = Request & { origin: {
+      url: string
+      body: object
+    } }
 
   export type Instance = {
     <R>(url: string, init?: Fetch2.RequestInit): Promise<R>
     interceptors: {
-      request: { use: InterceptorUse }
-      response: { use: InterceptorUse }
+      request: { use: InterceptorUseRequest }
+      response: { use: InterceptorUseResponse }
     }
   }
 }
 
 const _maxMethodLength = 'DELETE'.length - 1
 
-function _getRequest(prefix: string, url: string, params?: object, body?: object) {
+function _toRequest(prefix: string, config: Fetch2.Config): Fetch2.Request {
+  const { url, params, body } = config
   let method: Fetch2.Method = 'get'
   let _url = prefix
   let contentType = 'text/plain'
-  let _body: string | FormData | undefined
+  let _body: NodeJS.fetch.RequestInit['body']
 
   for (let i = 0; i < url.length; i++) {
     if (url[i] === ':') {
@@ -74,55 +94,79 @@ function _getRequest(prefix: string, url: string, params?: object, body?: object
     } else if (typeof body === 'object') {
       contentType = 'application/json'
       try {
-        _body = JSON.stringify(body)
+        _body = JSON.stringify(body) as NodeJS.fetch.RequestInit['body']
       } catch (err) {
         console.error(`${_url} body json stringify 失敗`, err)
-        _body = `數據非JSON，請檢查: ${String(body)}`
+        _body = `數據非JSON，請檢查: ${String(body)}` as NodeJS.fetch.RequestInit['body']
       }
     }
   }
 
+  const headers = {
+    'content-type': contentType,
+    ...config.headers,
+  }
+
   return {
+    ...config,
     method,
     url: _url,
     body: _body,
-    contentType,
+    headers,
   }
 }
 
 const createFetch2 = (options?: Fetch2.Options): Fetch2.Instance => {
   const { prefix = '' } = options || {}
   const interceptors = {
-    requestUses: [] as Fetch2.InterceptorUseCallback[],
-    responseUses: [] as Fetch2.InterceptorUseCallback[],
+    requestUses: [] as Fetch2.InterceptorUseRequestCallback[],
+    responseUses: [] as Fetch2.InterceptorUseResponseCallback[],
   }
 
   const newFetch = (async <R>(url: string, init?: Fetch2.RequestInit) => {
-    const { method, url: _url, contentType, body } = _getRequest(prefix, url, init?.params, init?.body)
-    const headers: Record<string, any> = {
-      'content-type': contentType,
+    let config: Fetch2.Config = {
+      ...init,
+      url,
     }
+
+    if (interceptors.requestUses.length) {
+      for (let i = 0; i < interceptors.requestUses.length; i++) {
+        config = interceptors.requestUses[i](config)
+      }
+    }
+
+    const request = _toRequest(prefix, config)
+    let res = {} as Fetch2.InterceptorResponse | Fetch2.InterceptorErrorResponse
 
     try {
-      const res= await fetch(_url, {
-        ...init,
-        method,
-        headers,
-        body,
-      })
-
-      return res[init?.resType || 'json']() as R
+      res = await fetch(request.url, request) as Fetch2.InterceptorResponse
+      ;(res as Fetch2.InterceptorResponse).data = await (res as Fetch2.InterceptorResponse)[config?.resType || 'json']()
     } catch (e) {
-
-      return undefined as R
+      res = {} as Fetch2.InterceptorErrorResponse
+    } finally {
+      res.req = request as Fetch2.ResReq
+      res.req.origin = {
+        url: config.url,
+        body: config.body as object,
+      }
     }
+
+    let result: R
+
+    if (interceptors.responseUses.length) {
+      for (let i = 0; i < interceptors.responseUses.length; i++) {
+        result = interceptors.responseUses[i](res)
+      }
+    }
+
+    return result!
   }) as Fetch2.Instance
 
-  const useRequest: Fetch2.InterceptorUse = (callback) => {
+  const useRequest: Fetch2.InterceptorUseRequest = (callback) => {
     interceptors.requestUses.push(callback)
   }
 
-  const useResponse: Fetch2.InterceptorUse = (callback) => {
+  const useResponse: Fetch2.InterceptorUseResponse = (callback) => {
     interceptors.responseUses.push(callback)
   }
 
