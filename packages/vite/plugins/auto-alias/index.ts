ecc265c815b45f2e3c171fb1825674c3f4762331
twjw <aaaawjwa@gmail.com>
2023-12-08 16:35:06 +0800

    chore: auto-alias 路徑更換

---
+++ packages/vite/plugins/auto-alias/index.ts
@@ -0,0 +1,81 @@
+import path from 'path'
+import { log } from '../../../../utils/log'
+import fs from 'fs'
+import jsonc from 'jsonc-parser'
+import type { Plugin } from 'vite'
+import { envConfigFilename, envConfigModuleFromName } from '../../../node/create-env-config/constants.ts'
+import { buildFolderName } from "../../../../constants";
+
+type AutoAliasOptions = {
+  filename?: string
+  filepath?: string /* absolute path */
+  hasEnv?: boolean
+  envModuleName?: string
+}
+
+function getIdeaPaths(filename: string, filepath: string) {
+  try {
+    const text = fs.readFileSync(filepath, { encoding: 'utf8' })
+    const paths = jsonc.parse(text)?.compilerOptions?.paths || {}
+    const dirReg = /\/\*$/
+    const result = {} as Record<string, any>
+
+    for (const k in paths) {
+      result[(k as string).replace(dirReg, '')] = `/${paths[k][0].replace(dirReg, '')}`
+    }
+
+    return result
+  } catch (error) {
+    log.error(error, `\n解析 ${filename} 失敗，取消該檔自動 alias`)
+    return {}
+  }
+}
+
+function createViteAliasFromTsconfig(options?: AutoAliasOptions) {
+  const {
+    filename = 'tsconfig.json',
+    filepath = path.resolve(process.cwd(), `./${filename}`),
+  } = options || {}
+  const alias = getIdeaPaths(filename, filepath)
+
+  log.info(`通過 ${filename} 生成的 alias:`)
+  log.info(alias)
+
+  if (options?.hasEnv) {
+    alias[options.envModuleName!] = `/node_modules/${buildFolderName}/${envConfigFilename}`
+    log.info(`vite-plugin-auto-alias 的 vite.resolve.alias name 為 ${options.envModuleName!}`)
+  }
+
+  return alias
+}
+
+function autoAlias(options?: AutoAliasOptions): any {
+  const _options = options || {}
+
+  if (_options.hasEnv == null) {
+    _options.hasEnv = true
+  }
+
+  if (_options.envModuleName == null) {
+    _options.envModuleName = envConfigModuleFromName
+  }
+
+  const plugin: Plugin = {
+    name: 'vite-plugin-auto-alias',
+    enforce: 'pre',
+    config() {
+      const alias = createViteAliasFromTsconfig(_options)
+
+      return {
+        resolve: {
+          alias,
+        },
+      }
+    },
+  }
+
+  return plugin
+}
+
+export type { AutoAliasOptions }
+export { autoAlias }
