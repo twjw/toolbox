e4525be5b7b598787dad267e7f79427df9b354bc
twjw <aaaawjwa@gmail.com>
2023-12-11 22:37:06 +0800

    feat: create-enum

---
+++ packages/common/create-enum/index.ts
@@ -0,0 +1,73 @@
+namespace Enum {
+	export type values<T extends readonly any[][]> = T[number] extends [string, ...infer Rest]
+		? T
+		: never
+}
+
+const _DEFAULT_DATA_KEYS = ['label', 'value'] as const
+
+function createEnum<KS extends readonly string[] | undefined, VS extends readonly any[][]>(
+	dataKeys: KS,
+	values: Enum.values<VS>,
+) {
+	type NumberKS = KS extends undefined
+		? (typeof _DEFAULT_DATA_KEYS)[number]
+		: (typeof _DEFAULT_DATA_KEYS)[number] | (KS & readonly string[])[number]
+
+	return {
+		_isInit: false,
+		_dataKeyIdxes: {} as Record<NumberKS, number>,
+		_labelIdxes: {} as Record<Enum.values<VS>[number][0], number>,
+		_valueIdxes: {} as Record<Enum.values<VS>[number][1], number>,
+		_init() {
+			if (this._isInit) return
+
+			const _dataKeys = (_DEFAULT_DATA_KEYS as any).concat(dataKeys || [])
+			for (let i = 0; i < values.length; i++) {
+				if (i === 0) {
+					for (let j = 0; j < values[i].length; j++) {
+						this._dataKeyIdxes[_dataKeys[j] as NumberKS] = j
+					}
+				}
+
+				this._labelIdxes[values[i][0] as Enum.values<VS>[number][0]] = i
+				this._valueIdxes[values[i][1] as Enum.values<VS>[number][1]] = i
+			}
+
+			this._isInit = true
+		},
+		getByLabel(label: Enum.values<VS>[number][0], dataKey?: NumberKS) {
+			this._init()
+			return values[this._labelIdxes[label]]?.[dataKey ? this._dataKeyIdxes[dataKey] : 1]
+		},
+		getByValue(value: Enum.values<VS>[number][1], dataKey?: NumberKS) {
+			this._init()
+			return values[this._valueIdxes[value]]?.[dataKey ? this._dataKeyIdxes[dataKey] : 1]
+		},
+		map<U>(callback: (value: Enum.values<VS>[number], index: number) => U): U[] {
+			this._init()
+
+			let result: U[] = []
+			for (let i = 0; i < values.length; i++) {
+				result.push(callback(values[i], i))
+			}
+
+			return result
+		},
+	}
+}
+
+export { createEnum }
+/* example
+const a = createEnum(
+	['color'] as const,
+	[
+		['ERROR', 1, 'red'],
+		['OK', 2, 'green'],
+	] as const
+)
+
+a.getByLabel('ERROR', 'color')
+a.getByValue(1, 'label')
+a.map(e => e[0])
+*/
--- packages/common/index.ts
+++ packages/common/index.ts
@@ -1,11 +1,19 @@
 export * from './create-callback-watcher'
+
+export * from './create-enum'
+
 export * from './create-fetch2/compete-enum'
 export * from './create-fetch2'
 export type * from './create-fetch2/type'
+
 export * from './create-log'
+
 export * from './create-uuid'
-export * from './enum2'
+
 export * from './range-loop'
+
 export * from './recur-find-key-str-value'
+
 export * from './recur-find-key-value'
+
 export * from './wait-ms'
