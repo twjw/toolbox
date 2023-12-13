7eea8f0bbeea7ffbd08f0eefff4f24662c598013
twjw <aaaawjwa@gmail.com>
2023-12-13 21:45:25 +0800

    refactor(wtbx-i18n):  remove register function

--- packages/vite/plugins/react/wtbx-i18n/index.ts
+++ packages/vite/plugins/react/wtbx-i18n/index.ts
@@ -2,7 +2,7 @@ import type { Plugin } from 'vite'
 import fs from 'fs'
 import path from 'path'
 import { log } from '../../../../../utils/log.ts'
-import {waitMs} from "../../../../common";
+import { waitMs } from '../../../../common'
 
 type WtbxI18nOptions = {
 	dirs: string[] // 字典檔目錄絕對路徑列表(後蓋前)
@@ -15,57 +15,61 @@ const FULL_PLUGIN_NAME = `vite-plugin-${PLUGIN_NAME}`
 const V_MODULE_NAME = `~${PLUGIN_NAME}`
 const V_MODULE_ID = `@@${V_MODULE_NAME}.jsx`
 
-function _generateLangGlobPath ({ dirs }: Required<WtbxI18nOptions>) {
-  const globMap = {} as _GlobMap
-  const matchExtReg = /\.(ts|json)$/
+function _generateLangGlobPath({ dirs }: Required<WtbxI18nOptions>) {
+	const globMap = {} as _GlobMap
+	const matchExtReg = /\.(ts|json)$/
 
-  try {
-    const filePathMap = {} as Record<string, 1> // <relativeFilepath>
+	try {
+		const filePathMap = {} as Record<string, 1> // <relativeFilepath>
 
-    for (let i = 0; i < dirs.length; i++) {
-      const dir = dirs[i]
-      const files = fs.readdirSync(dir)
+		for (let i = 0; i < dirs.length; i++) {
+			const dir = dirs[i]
+			const files = fs.readdirSync(dir)
 
-      for (let j = 0; j < files.length; j++) {
-        const file = files[j]
-        const filepath = path.join(dir, file);
-        const stat = fs.lstatSync(filepath)
+			for (let j = 0; j < files.length; j++) {
+				const file = files[j]
+				const filepath = path.join(dir, file)
+				const stat = fs.lstatSync(filepath)
 
-        if (stat.isFile() && matchExtReg.test(file)) {
-          filePathMap[path.relative(process.cwd(), filepath)] = 1
-        }
-      }
-    }
+				if (stat.isFile() && matchExtReg.test(file)) {
+					filePathMap[path.relative(process.cwd(), filepath)] = 1
+				}
+			}
+		}
 
-    for (const relativeFilepath in filePathMap) {
-      const globPath = `/${relativeFilepath.replace(/[\\]/g, '/')}`
-      const filename = globPath.match(/[^\\/]+$/)?.[0]!
+		for (const relativeFilepath in filePathMap) {
+			const globPath = `${relativeFilepath.replace(/[\\]/g, '/')}`
+			const filename = globPath.match(/[^\\/]+$/)?.[0]!
 
-      globMap[filename.replace(matchExtReg, '')] = `${globPath}`
-    }
+			globMap[filename.replace(matchExtReg, '')] = `./${globPath}`
+		}
 
-    return globMap
-  } catch (error) {
-    log.error(`${FULL_PLUGIN_NAME} 讀取字典檔失敗`)
-    log.error(error)
-    process.exit(0)
-  }
+		return globMap
+	} catch (error) {
+		log.error(`${FULL_PLUGIN_NAME} 讀取字典檔失敗`)
+		log.error(error)
+		process.exit(0)
+	}
 }
 
-function _generateStringModule ({ globMap }: { globMap: _GlobMap }) {
-  const firstLocale = Object.keys(globMap)[0]
+function _generateStringModule({ globMap }: { globMap: _GlobMap }) {
+	const firstLocale = Object.keys(globMap)[0]
+	const firstLocaleStr = firstLocale ? `'${firstLocale}'` : null
 
-  const mapToList = (kv: 'keys' | 'values') => `[${Object[kv](globMap).map(e => `'${e}'`).join(', ')}]`
-
-  return `
+	return `
 import { useState, useEffect, Fragment } from 'react'
 import { recurFindKeyStrValue } from 'wtbx/common'
 
-let _globMap = import.meta.glob(${mapToList('values')}) // Record{string, Promise{any} | any} 字典檔
-const _dictionaryMap = {} // Record{string, Promise{any} | any} _globMap 轉換 key 為 locale 塞入的字典檔
+const _dictionaryMap = {
+  ${Object.entries(globMap)
+		.map(([locale, path]) => `'${locale}': () => import('${path}')`)
+		.join(',\n')}
+} // Record{string, Promise{any} | any} _globMap 轉換 key 為 locale 塞入的字典檔
 let dictionary = {} // 當前字典
 
-const localeList = ${mapToList('keys')} // string[] 項目的語系列表
+const localeList = ${`[${Object.keys(globMap)
+		.map(e => `'${e}'`)
+		.join(', ')}]`} // string[] 項目的語系列表
 let locale = localeList[0] // 當前語系
 
 let _forceUpdate // 強刷 APP 組件
@@ -91,110 +95,104 @@ async function _updateLocale(_locale) {
   locale = _locale
 }
 
+function _notFoundLocaleWarn () {
+  console.warn(\`not found locale \${_locale}\`)
+}
+
 async function setLocale(_locale) {
   if (_dictionaryMap[_locale] == null) {
-    console.warn(\`not found locale \${_locale}\`)
+    _notFoundLocaleWarn()
     return
   }
   
-  await _updateLocale(_locale, _locale)
+  await _updateLocale(_locale)
   _forceUpdate?.()
 }
 
-function register({ default: defaultLocale = ${firstLocale ? `'${firstLocale}'` : null} }) {
-  if (defaultLocale == null) {
-    throw new Error('[~wtbx-i18n] not found locale')  
-  }
-  
-  let i = 0
-  for (const globPath in _globMap) {
-    _dictionaryMap[localeList[i++]] = _globMap[globPath]
-  }
-  
-  locale = defaultLocale
-  _globMap = null
-}
-
-function App({ fallback, children }) {
+function App({ defaultLocale, fallback, children }) {
   const [i, update] = useState(0)
   
   useEffect(() => {
-    _updateLocale(locale).then(() => {
+    const hasAny = 
+      (locale = defaultLocale || ${firstLocaleStr}) != null 
+        && _dictionaryMap[locale] != null
+
+    if (hasAny) {
       _forceUpdate = () => update(i => i + 1)
-      _forceUpdate()
-    })
+      _updateLocale(locale).then(() => {
+        _forceUpdate()
+      })
+    } else {
+      _notFoundLocaleWarn()
+    }
   }, [])
   
   if (i === 0) return fallback || <></>
-  return <Fragment key={locale}>{children}</Fragment>
+  return <Fragment key={i}>{children}</Fragment>
 }
 
-export { dictionary, locale, t, register, setLocale, App }
+export { dictionary, locale, t, setLocale, App }
 `
 }
 
 function wtbxI18n(options: WtbxI18nOptions): any {
-  const { dirs } = options || {}
-  let globMap: _GlobMap = {} // [[relativePath, filename(no-ext)], ...[]]
+	const { dirs } = options || {}
+	let globMap: _GlobMap = {} // [[relativePath, filename(no-ext)], ...[]]
 
-  const plugin: Plugin = {
-    name: FULL_PLUGIN_NAME,
-    enforce: 'pre',
-    configResolved() {
-      globMap = _generateLangGlobPath({ dirs })
-      log.info(`已開啟多語系功能，模塊名稱為 ${V_MODULE_NAME}...`)
-    },
-    configureServer(server) {
-      let isUpdating = false
+	const plugin: Plugin = {
+		name: FULL_PLUGIN_NAME,
+		enforce: 'pre',
+		configResolved() {
+			globMap = _generateLangGlobPath({ dirs })
+			log.info(`已開啟多語系功能，模塊名稱為 ${V_MODULE_NAME}...`)
+		},
+		configureServer(server) {
+			let isUpdating = false
 
-      async function debounceGenerate (filepath: string) {
-        let filename = null as string | null
+			async function debounceGenerate(filepath: string) {
+				let filename = null as string | null
 
-        for (let i = 0; i < dirs.length; i++) {
-          const [, _filename] = filepath.split(dirs[i])
-          if (_filename != null) filename = _filename
-        }
+				for (let i = 0; i < dirs.length; i++) {
+					const [, _filename] = filepath.split(dirs[i])
+					if (_filename != null) filename = _filename
+				}
 
-        if (isUpdating || !filename || !/^\\[A-z0-9-_]+\.(ts|json)$/.test(filename)) return
+				if (isUpdating || !filename || !/^\\[A-z0-9-_]+\.(ts|json)$/.test(filename)) return
 
-        isUpdating = true
-        globMap = _generateLangGlobPath({ dirs })
-        await waitMs(250)
-        isUpdating = false
+				isUpdating = true
+				globMap = _generateLangGlobPath({ dirs })
+				await waitMs(250)
+				isUpdating = false
 
-        const mod = server.moduleGraph.getModuleById(V_MODULE_ID)
+				const mod = server.moduleGraph.getModuleById(V_MODULE_ID)
 
-        if (mod) {
-          server.moduleGraph.invalidateModule(mod)
-          server.ws.send({
-            type: 'full-reload'
-          })
-        }
-      }
+				if (mod) {
+					server.moduleGraph.invalidateModule(mod)
+					server.ws.send({
+						type: 'full-reload',
+					})
+				}
+			}
 
-      server.watcher.on('unlink', debounceGenerate)
-      server.watcher.on('add', debounceGenerate)
-    },
-    resolveId(id) {
-      if (id === V_MODULE_NAME) {
-        return V_MODULE_ID;
-      }
-    },
-    load(id) {
-      if (id === V_MODULE_ID) {
-        if (Object.keys(globMap).length === 0) return
-        return _generateStringModule({ globMap })
-      }
-    }
-  }
+			server.watcher.on('unlink', debounceGenerate)
+			server.watcher.on('add', debounceGenerate)
+		},
+		resolveId(id) {
+			if (id === V_MODULE_NAME) {
+				return V_MODULE_ID
+			}
+		},
+		load(id) {
+			if (id === V_MODULE_ID) {
+				if (Object.keys(globMap).length === 0) return
+				return _generateStringModule({ globMap })
+			}
+		},
+	}
 
-  return plugin
+	return plugin
 }
 
-export type {
-  WtbxI18nOptions,
-}
+export type { WtbxI18nOptions }
 
-export {
-  wtbxI18n,
-}
+export { wtbxI18n }
--- packages/vite/plugins/react/wtbx-i18n/type.ts
+++ packages/vite/plugins/react/wtbx-i18n/type.ts
@@ -1,16 +1,19 @@
-import type {FC, ReactNode} from 'react'
-import type {WObject} from "../../../../type";
+import type { ReactNode } from 'react'
+import type { WObject } from '../../../../type'
 
 namespace WtbxI18n {
-  export type Translate = <Dictionary extends Record<string, any>>(key: WObject.RecursiveKeyOf<Dictionary>, value?: string[]) => string
+	export type Translate<Dictionary extends Record<string, any>> = (
+		key: WObject.RecursiveKeyOf<Dictionary>,
+		value?: string[],
+	) => string
 
-  export type Register = <Locale>(options: { default: Locale }) => void
+	export type SetLocale<Locale> = (locale: Locale) => Promise<void>
 
-  export type SetLocale = <Locale>(locale: Locale) => Promise<void>
-
-  export type App = FC<{ fallback?: ReactNode, children: ReactNode }>
+	export type App<Locale> = (props: {
+		defaultLocale?: Locale
+		fallback?: ReactNode
+		children: ReactNode
+	}) => ReactNode
 }
 
-export type {
-  WtbxI18n
-}
+export type { WtbxI18n }
