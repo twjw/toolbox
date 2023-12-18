e97d639941e8e96c80bb108430ce2f01ec36be1f
twjw <aaaawjwa@gmail.com>
2023-12-18 17:22:13 +0800

    chore

--- packages/vite/plugins/react/page-routes/type.ts
+++ packages/vite/plugins/react/page-routes/type.ts
@@ -5,7 +5,7 @@ namespace ReactPageRoutes {
     Wrap: FC<{ children: ReactNode }>
   }) => ReactNode
 
-  export type UsePageRute<T = undefined> = (fullPath?: string) => { path: string } & { meta: T }
+  export type UsePageRute<Meta = undefined> = (fullPath?: string) => { path: string } & { meta: Meta }
 }
 
 export type {
