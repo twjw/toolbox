d43acb93e533a8ec45ba482deab8a1331df66199
twjw <aaaawjwa@gmail.com>
2023-12-29 20:04:36 +0800

    fix(0.1.5): dist rename to esm 以解決 webstorm 不會自動 import 的錯誤

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ const packages = ['common', 'node', 'react', 'type', 'vite', 'web']
 
 tsup.build({
 	entry: packages.map(e => `./packages/${e}/index.ts`),
-	outDir: 'dist',
+	outDir: 'esm',
 	format: ['esm' /*'cjs'*/],
 	splitting: true,
 	clean: true,
