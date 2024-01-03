36361cc96eea649f2e7b8ea447012cd531424f1e
twjw <aaaawjwa@gmail.com>
2024-01-03 11:31:38 +0800

    fix(0.2.9): 修正 ReactPageRoutes.CreatePageRoutes Wrap 類型錯誤

--- packages/vite/plugins/react/page-routes/type.ts
+++ packages/vite/plugins/react/page-routes/type.ts
@@ -1,19 +1,16 @@
-import type { FC, ReactNode } from "react";
+import type { FC, ReactNode } from 'react'
 
 namespace ReactPageRoutes {
-  export type CreatePageRoutes = (props: {
-    Wrap: FC<{ children: ReactNode }>
-  }) => ReactNode
+	export type CreatePageRoutes = (props: {
+		Wrap: FC<{ path: string; children: ReactNode }>
+	}) => ReactNode
 
-  export type UsePageRute<Meta = undefined> = () => { path: string } & { meta: Meta }
+	export type UsePageRute<Meta = undefined> = () => { path: string } & { meta: Meta }
 }
 
 type ReactPageRoutesOptions = {
-  defaultMeta?: any // 預設的 meta 資料
-  pages: string[] // 頁面目錄的絕對路徑(後蓋前)
+	defaultMeta?: any // 預設的 meta 資料
+	pages: string[] // 頁面目錄的絕對路徑(後蓋前)
 }
 
-export type {
-  ReactPageRoutesOptions,
-  ReactPageRoutes
-}
+export type { ReactPageRoutesOptions, ReactPageRoutes }
