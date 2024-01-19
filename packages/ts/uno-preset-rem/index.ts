debe1588c6735f821b1061981cc8480a712dc1cb
twjw <aaaawjwa@gmail.com>
2024-01-19 10:02:11 +0800

    uno 拆出去

---
+++ packages/ts/uno-preset-rem/index.ts
@@ -0,0 +1,21 @@
+import { definePreset } from '@unocss/core'
+
+const remRE = /(-?[.\d]+)rem/g
+
+/// 將值轉換為對應的數字 rem，比方說 unoPresetRemBaseFontSize 10, text-1 = 0.1rem
+const presetRem = definePreset((options?: { baseFontSize: number }) => {
+  const { baseFontSize = 16 } = options || {}
+
+  return {
+    name: '@unocss/preset-rem',
+    postprocess: util => {
+      util.entries.forEach(e => {
+        const value = e[1]
+        if (typeof value === 'string' && remRE.test(value))
+          e[1] = value.replace(remRE, (_, p1) => `${(p1 / baseFontSize) * 4}rem`)
+      })
+    },
+  }
+})
+
+export { presetRem }
