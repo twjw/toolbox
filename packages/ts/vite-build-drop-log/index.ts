a6a7b11b93bfc664e24b5eb2f7dc8b0572a3caa2
twjw <aaaawjwa@gmail.com>
2024-01-19 10:07:43 +0800

    vite-build-drop-log 拆出去

---
+++ packages/ts/vite-build-drop-log/index.ts
@@ -0,0 +1,26 @@
+import type { Plugin } from 'vite'
+
+const PLUGIN_NAME = '@wtbx/vite-build-drop-log'
+const FULL_PLUGIN_NAME = `vite-plugin-${PLUGIN_NAME}`
+const CONSOLE_NAME = `[${PLUGIN_NAME}]`
+
+function buildDropLog(): any {
+  const plugin: Plugin = {
+    name: FULL_PLUGIN_NAME,
+    enforce: 'pre',
+    config(_, { command }) {
+      if (command === 'build') {
+        console.log(`[LOG]${CONSOLE_NAME} 已開啟 console 清除功能`)
+        return {
+          esbuild: {
+            drop: ['console', 'debugger'],
+          },
+        }
+      }
+    },
+  }
+
+  return plugin
+}
+
+export { buildDropLog }
