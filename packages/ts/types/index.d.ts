2f02a09e5279b4ba13e62e5598f78d5e3f03a79d
twjw <aaaawjwa@gmail.com>
2024-02-29 17:02:38 +0800

    wtbx-types@1.0.3: 將 namespace 攤平

--- packages/ts/types/index.d.ts
+++ packages/ts/types/index.d.ts
@@ -1,73 +1,90 @@
-export namespace WUni {
-  // 兩個 union type 取交集
-  type Intersection<T, U> = T extends U ? T : never
+/** @description 兩個 union type 取交集 */
+export type UnionIntersection<T, U> = T extends U ? T : never
 
-  // ToTuple START
-  type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
-      k: infer I,
-    ) => void
-    ? I
-    : never
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+    k: infer I,
+  ) => void
+  ? I
+  : never
 
-  type LastOf<T> = UnionToIntersection<T extends any ? () => T : never> extends () => infer R
-    ? R
-    : never
+type LastOf<T> = UnionToIntersection<T extends any ? () => T : never> extends () => infer R
+  ? R
+  : never
 
-  type Push<T extends any[], V> = [...T, V]
+type Push<T extends any[], V> = [...T, V]
 
-  type ToTuple<T, L = LastOf<T>, N = [T] extends [never] ? true : false> = true extends N
-    ? []
-    : Push<ToTuple<Exclude<T, L>>, L>
-  // ToTuple END
+/** @description union 變成 tuple */
+export type UnionToTuple<T, L = LastOf<T>, N = [T] extends [never] ? true : false> = true extends N
+  ? []
+  : Push<UnionToTuple<Exclude<T, L>>, L>
 
-  export {
-    Intersection,
-    ToTuple,
-  }
+/** @description 物件key轉成 union string
+ * @example RecursiveKeyOf<{a: {b: string}, c: number}> -> 'a.b' | 'c' */
+export type RecursiveKeyOf<Obj extends object, Sep extends string = '.'> = {
+  [K in keyof Obj & (string | number)]: Obj[K] extends object
+    ? `${K}` | `${K}${Sep}${RecursiveKeyOf<Obj[K]>}`
+    : `${K}`
+}[keyof Obj & (string | number)]
+
+/** @description 移除 key 前綴(Prefix 2參) */
+export type IgnoreKeyPrefix<Obj extends object, Prefix extends string = '_'> = {
+  [K in keyof Obj as K extends `${Prefix}${infer Rest}` ? Rest : K]: Obj[K] extends object
+    ? IgnoreKeyPrefix<Obj[K], Prefix>
+    : Obj[K]
 }
 
-export namespace WObj {
-  type RecursiveKeyOf<Obj extends object, Sep extends string = '.'> = {
-    [K in keyof Obj & (string | number)]: Obj[K] extends object
-      ? `${K}` | `${K}${Sep}${RecursiveKeyOf<Obj[K]>}`
-      : `${K}`
-  }[keyof Obj & (string | number)]
-
-  type IgnoreKeyPrefix<Obj extends object, Prefix extends string = '_'> = {
-    [K in keyof Obj as K extends `${Prefix}${infer Rest}` ? Rest : K]: Obj[K] extends object
-      ? IgnoreKeyPrefix<Obj[K], Prefix>
-      : Obj[K]
-  }
-
-  type DeepPartial<Obj extends Record<string, any>> = {
-    [K in keyof Obj]?: Obj[K] extends Record<string, any> ? DeepPartial<Obj[K]> : Obj[K]
-  }
-
-  // BetterPick START
-  type BetterPickKey<T extends Record<string | number, any>, K = (keyof T & string | number) | `${keyof T & string | number}->${string}`> =
-    K | `?${K & string | number}`
-
-  type BetterPick<T extends Record<string | number, any>, K extends BetterPickKey<T>> = {
-    [P in (K extends `${infer PS}${infer K1}->${string}` ? PS extends '?' ? never : `${PS}${K1}` : K extends `?${string}` ? never : K) as K extends `?${P & string | number}->${infer K2}` ? K2 : P]: T[P]
-  } & {
-    [P in (K extends `${infer PS}${infer K1}->${string}` ? PS extends '?' ? K1 : never : K extends `?${infer K1}` ? K1 : never) as K extends `?${P & string | number}->${infer K2}` ? K2 : P]?: T[P]
-  }
-  // ToTuple END
-
-  export {
-    RecursiveKeyOf,
-    IgnoreKeyPrefix,
-    DeepPartial,
-    BetterPick,
-  }
+/** @description 深可選 */
+export type DeepPartial<Obj extends Record<string, any>> = {
+  [K in keyof Obj]?: Obj[K] extends Record<string, any> ? DeepPartial<Obj[K]> : Obj[K]
 }
 
-export namespace WStr {
-  type FirstUppercase<Str extends string> = Str extends `${infer First}${infer Rest}`
-    ? `${Uppercase<First>}${Rest}`
-    : never
+type BetterPickKey<
+  T extends Record<string | number, any>,
+  K = (keyof T & string | number) | `${keyof T & string | number}->${string}`
+> =
+  K | `?${K & string | number}`
 
-  export {
-      FirstUppercase,
-  }
+/** @description TS 自帶的 Pick 加強版，支持更名與轉可選
+ * @example BetterPick<{a: 1, b: 2, c: 3, d: 4}, 'a' | '?b' | 'c->c2' | '?d->d2'> -> {a: 1, b?: 2, c2: 3, d2?: 4} */
+export type BetterPick<
+  T extends Record<string | number, any>,
+  K extends BetterPickKey<T>
+> = {
+  // 必填
+  [
+    P in (
+      K extends `${infer PS}${infer K1}->${string}`
+        ? PS extends '?'
+          ? never
+          : `${PS}${K1}`
+        : K extends `?${string}`
+          ? never
+          : K
+      ) as
+      K extends `?${P & string | number}->${infer K2}`
+        ? K2
+        : P
+  ]: T[P]
 }
+// 非必填
+& {
+  [
+    P in (
+      K extends `${infer PS}${infer K1}->${string}`
+        ? PS extends '?'
+          ? K1
+          : never
+        : K extends `?${infer K1}`
+          ? K1
+          : never
+      ) as
+      K extends `?${P & string | number}->${infer K2}`
+        ? K2
+        : P
+  ]?: T[P]
+}
+
+/** @description 首字串變大寫 */
+export type FirstUppercase<Str extends string> = Str extends `${infer First}${infer Rest}`
+  ? `${Uppercase<First>}${Rest}`
+  : never
