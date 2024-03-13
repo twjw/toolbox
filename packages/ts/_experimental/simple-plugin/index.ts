393a00861f52646905519c3e5eed16172b1a1c2e
twjw <aaaawjwa@gmail.com>
2024-03-13 10:30:06 +0800

    配置 husky 並移除 package 的 prettier 以及測試 husky

--- packages/ts/_experimental/simple-plugin/index.ts
+++ packages/ts/_experimental/simple-plugin/index.ts
@@ -31,7 +31,6 @@
 //
 // lib()
 
-
 // function create <LC extends string[], F extends Function>(lifecycleList: LC, lib: F) {
 // function result () {
 //
@@ -45,61 +44,61 @@
 // const lib = create(['req', 'res'] as const, () => {})
 // lib.use('req')
 
+function simplePlugin<LF extends Record<string, Function>, R>(lib: (lfObj: LF) => R) {
+	const lfListObj = {} as {
+		[K in keyof LF]: LF[K][]
+	}
 
-function simplePlugin <LF extends Record<string, Function>, R>(lib: (lfObj: LF) => R) {
-  const lfListObj = {} as {
-    [K in keyof LF]: LF[K][]
-  }
+	function _transToLfObj() {
+		const lfObj = {} as {
+			[K in keyof LF]: LF[K]
+		}
 
-  function _transToLfObj () {
-    const lfObj = {} as {
-      [K in keyof LF]: LF[K]
-    }
+		for (let k in lfListObj) {
+			lfObj[k] = function (...args) {
+				let result
+				for (let i = 0; i < lfListObj[k].length; i++) {
+					result = lfListObj[k][i](...args)
+				}
+				return result
+			}
+		}
 
-    for (let k in lfListObj) {
-      lfObj[k] = function (...args) {
-        let result
-        for (let i = 0; i < lfListObj[k].length; i++) {
-          result = lfListObj[k][i](...args)
-        }
-        return result
-      }
-    }
+		return lfObj
+	}
 
-    return lfObj
-  }
+	function result() {
+		return lib(_transToLfObj())
+	}
 
-  function result () {
-    return lib(_transToLfObj())
-  }
+	result.middleware = function middleware() {}
 
-  result.middleware = function middleware () {
+	result.use = function use<K extends keyof LF>(
+		name: K,
+		impl: LF[K] extends (...args: infer Args) => infer R ? (...args: Args) => R : never,
+	): void {
+		if (lfListObj[name]) lfListObj[name].push(impl as LF[K])
+		else lfListObj[name] = [impl as LF[K]]
+	}
 
-  }
-
-  result.use = function use <K extends keyof LF>(name: K, impl: LF[K] extends (...args: infer Args) => infer R ? (...args: Args) => R : never): void {
-    if (lfListObj[name]) lfListObj[name].push(impl as LF[K])
-    else lfListObj[name] = [impl as LF[K]]
-  }
-
-  return result
+	return result
 }
 
 const lib = simplePlugin<
-  {
-    req: (origin: string) => string
-  },
-  string
+	{
+		req: (origin: string) => string
+	},
+	string
 >(({ req }) => {
-  return req('123')
+	return req('123')
 })
 
-lib.use('req', (origin) => {
-  return origin + (Number(origin[origin.length - 1]) + 1)
+lib.use('req', origin => {
+	return origin + (Number(origin[origin.length - 1]) + 1)
 })
 
-lib.use('req', (origin) => {
-  return origin + (Number(origin[origin.length - 1]) + 1)
+lib.use('req', origin => {
+	return origin + (Number(origin[origin.length - 1]) + 1)
 })
 
 console.log(lib())
