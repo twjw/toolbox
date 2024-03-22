8f4b75d74739d8a003b48ea15d9c4444ceda53e4
twjw <aaaawjwa@gmail.com>
2024-03-22 14:07:25 +0800

    移除復寫 jsx 函數

--- packages/ts/wrap-jsx/jsx-dev-runtime/index.js
+++
@@ -1,7 +0,0 @@
-import { Fragment, jsxDEV as jsxDEV_ } from 'react/jsx-dev-runtime'
-import { newJsx } from '../utils.js'
-
-const map = {}
-const jsxDEV = newJsx(jsxDEV_, map)
-
-export { Fragment, jsxDEV, map }
--- packages/ts/wrap-jsx/jsx-runtime/index.js
+++
@@ -1,8 +0,0 @@
-import { Fragment, jsx as jsx_, jsxs as jsxs_ } from 'react/jsx-runtime'
-import { newJsx } from '../utils.js'
-
-const map = {}
-const jsx = newJsx(jsx_, map)
-const jsxs = newJsx(jsxs_, map)
-
-export { Fragment, jsx, jsxs, map }
