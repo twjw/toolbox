18ab851cac5bc5b48c545f32ee35f9e07d6218eb
twjw <aaaawjwa@gmail.com>
2024-06-26 16:17:39 +0800

    wtbx-vite-react-i18n@2.0.0 最基本的合併字典功能完成

---
+++ __test__scripts__/test-script-1.ts
@@ -0,0 +1,121 @@
+import path from 'node:path'
+import fs from 'node:fs'
+import { fileURLToPath } from 'url'
+
+type Dictionaries = {
+	[key: string]: string | Dictionaries
+}
+
+type DictionaryMap = Record<string, { dir: string; names: string[] }>
+
+const SL = path.normalize('/')
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+async function recursiveFindPaths(dirPath: string, result: string[] = []) {
+	const pathList = await fs.promises.readdir(dirPath, { withFileTypes: true })
+
+	for (let i = 0; i < pathList.length; i++) {
+		const e = pathList[i]
+		const filepath = path.join(dirPath, e.name)
+
+		if (e.isDirectory()) {
+			await recursiveFindPaths(filepath, result)
+		} else if (/\.json$/.test(e.name)) {
+			result.push(filepath)
+		}
+	}
+
+	return result
+}
+
+function transformSamePathMap(filepathList: string[], dirs: string[]) {
+	const filepathMap: DictionaryMap = {}
+
+	if (dirs.length === 0) return filepathMap
+
+	for (let i = 0; i < filepathList.length; i++) {
+		const filepath = filepathList[i]
+		if (dirs.length > 0) {
+			for (let j = 0; j < dirs.length; j++) {
+				const dir = dirs[j]
+				let isBreak = false
+
+				for (let k = 0; k < dir.length; k++) {
+					const dirTxt = dir[k]
+
+					if (filepath[k] !== dirTxt) {
+						break
+					} else if (k === dir.length - 1) {
+						const relativeFilepath = filepath.substring(dir.length)
+
+						filepathMap[relativeFilepath] = {
+							dir,
+							names: relativeFilepath
+								.substring(SL.length, relativeFilepath.length - 5)
+								.split(SL)
+								.filter(e => e !== '_'),
+						}
+						isBreak = true
+						break
+					}
+				}
+
+				if (isBreak) break
+			}
+		} else {
+			filepathMap[filepath] = {
+				dir: dirs[0],
+				names: [],
+			}
+		}
+	}
+
+	return filepathMap
+}
+
+async function mergeDictionaries(dictMap: DictionaryMap, dictionaries?: Dictionaries) {
+	const _dictionaries: Dictionaries = dictionaries || {}
+
+	for (let relativeFilepath in dictMap) {
+		const { dir, names } = dictMap[relativeFilepath]
+		const filepath = dir + relativeFilepath
+
+		try {
+			const dict = JSON.parse(await fs.promises.readFile(filepath, 'utf-8'))
+			let node = _dictionaries
+			for (let key in dict) {
+				for (let i = 0; i < names.length; i++) {
+					const name = names[i]
+					if (node[name] == null) node[name] = {}
+					node = node[name] as Dictionaries
+				}
+				node[key] = dict[key]
+			}
+		} catch {}
+	}
+
+	return _dictionaries
+}
+
+run()
+async function run() {
+	const limitLocales = [] as string[] | undefined
+	const dirs = [
+		path.resolve(__dirname, '../packages/ts/vite-react-i18n/example/base/dictionaries/one'),
+		path.resolve(__dirname, '../packages/ts/vite-react-i18n/example/base/dictionaries/two'),
+	]
+	const filepathList = (await Promise.all(dirs.map(e => recursiveFindPaths(e)))).flat()
+	const dictMap = transformSamePathMap(filepathList, dirs)
+	const hasLimitLocales = limitLocales != null && limitLocales.length > 0
+	let locales: string[] = []
+	const dictionaries: Dictionaries = await mergeDictionaries(dictMap)
+
+	if (hasLimitLocales) {
+		locales = limitLocales
+	} else {
+		// locales =
+	}
+
+	console.log(dictMap)
+	console.log(JSON.stringify(dictionaries, null, 2))
+}
