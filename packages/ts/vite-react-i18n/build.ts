6b5c50d16e57c3d3ab5bca6296423aef5a51cb33
twjw <aaaawjwa@gmail.com>
2024-07-31 14:19:50 +0800

    wtbx-vite-react-i18n@2.0.0 新增構建腳本

---
+++ packages/ts/vite-react-i18n/build.ts
@@ -0,0 +1,38 @@
+import util from 'node:util'
+import fs from 'node:fs/promises'
+import { exec } from 'node:child_process'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const promiseExec = util.promisify(exec)
+
+;(async () => {
+	console.log('開始編譯代碼\n')
+
+	try {
+		const { stdout } = await promiseExec(
+			'tsup-node src/index.ts --platform node --format esm --dts --clean',
+		)
+		console.log(stdout)
+	} catch (error) {
+		console.error(error)
+	}
+
+	console.log('開始檢查創建 dist/templates')
+
+	try {
+		await fs.access(path.join(__dirname, 'dist/templates'))
+	} catch {
+		await fs.mkdir(path.join(__dirname, 'dist/templates'))
+	}
+
+	console.log('將模板類型拷貝至 dist')
+
+	await fs.copyFile(
+		path.join(__dirname, 'src/templates/client.d.ts'),
+		path.join(__dirname, 'dist/templates/client.d.ts'),
+	)
+
+	console.log('構建完成')
+})()
